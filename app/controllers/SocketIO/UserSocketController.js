4caf0140b5ce87db795900977fb41e4d16ee9ef4
unknown <istiaq.me@gmail.com>
2023-02-27 04:27:42 +0600

    Socket IO With Multi Event Emitting from Single Controller

--- app/controllers/SocketIO/UserSocketController.js
+++ app/controllers/SocketIO/UserSocketController.js
@@ -5,19 +5,24 @@ module.exports = {
     list : async function (ws) {
         try{
             const users = await UserService.list();
-            return ws.io.emit(ws.event, response.build('User List Loaded', {users}, 200)); // wrap data in object to avoid confusion
+            ws.io.emit(ws.event, response.build('User List Loaded', {users}, 200)); // wrap data in object to avoid confusion
+            ws.io.emit('some/other/event', response.build('Some Other Event Emitted', {
+                lulu: true,
+                userCount: users.length,
+                usersAreHero: false,
+            }, 200)); // wrap data in object to avoid confusion
         }
         catch(error){
-            return ws.io.emit(ws.event, Handler(error));
+            ws.io.emit(ws.event, Handler(error));
         }
     },
     privateProfileDetails : async function (ws) {
         try{
             const user = await UserService.details(ws.payload.privateId);
-            return ws.io.emit(ws.event, response.build('Private User Details Loaded.', {user}, 200)); // wrap data in object to avoid confusion
+            ws.io.emit(ws.event, response.build('Private User Details Loaded.', {user}, 200)); // wrap data in object to avoid confusion
         }
         catch(error){
-            return ws.io.emit(ws.event, Handler(error));
+            ws.io.emit(ws.event, Handler(error));
         }
     }
 }
