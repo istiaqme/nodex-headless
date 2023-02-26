8179ae4af4dc867816a71b92b2bc58ab51043a49
unknown <istiaq.me@gmail.com>
2023-02-27 05:49:19 +0600

    Added Configuration System for Event Emitter

--- app/responses/Event.js
+++ app/responses/Event.js
@@ -1,7 +1,42 @@
+const ValidationError = lulu.use('app/errors/ValidationError');
 module.exports = {
-    emit: function (eventName, data) {
-        if(lulu.context.ws.io){
-            lulu.context.ws.io.emit(eventName, data);
+    emit: function (eventName, data, config = null) {
+        if(lulu.context.ws){
+            if(config){
+                if(config.broadcast){
+                    // to all clients in the current namespace except the sender
+                    lulu.context.ws.socket.broadcast.emit(eventName, data);
+                }
+                else if(config.room){
+                    if(config.room.method == 'to'){
+                        // to all clients in the given room except the sender
+                        lulu.context.ws.io.to(config.room.name).emit(eventName, data);
+                    }
+                    else if(config.room.method == 'in'){
+                        // to all clients in the given room including the sender
+                        lulu.context.ws.io.in(config.room.name).emit(eventName, data);
+                    }
+                    else{
+                        throw new Error('Invalid room method. Please use "to" or "in"');
+                    }
+                }
+                else if(config.socketId){
+                    // to a specific client
+                    lulu.context.ws.io.to(config.socketId).emit(eventName, data);
+                }
+                else if(volatile){
+                    // to all clients in the current namespace except the sender
+                    lulu.context.ws.socket.volatile.emit(eventName, data);
+                }
+                else{
+                    lulu.context.ws.socket.emit(eventName, data);
+                }
+            }
+            else{
+                lulu.context.ws.socket.emit(eventName, data);
+            }
         }
     }
+
+    // Currently supports only basic emmiting. Will add support for multiple rooms and namespaces later.
 }
